7a16eecb4ed935204ff38124fff25831711963e5
evixamar <evixamar@gmail.com>
2019-02-14 13:18:27 -0500

    added backgroung

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+// let currentJoke = {}
+const getWeather = () => {
+  // send the request
+  fetch(
+    'http://api.openweathermap.org/data/2.5/weather?q=tampa&appid=15174363504cfc356b52cf95eb141093&units=imperial'
+  )
+    //`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15174363504cfc356b52cf95eb141093&units=imperial`
+    // getting the response back
+    .then(resp => {
+      return resp.json()
+    })
+    .then(joke => {
+      console.log(joke)
+      console.log('Humidity: ' + joke.main.temp_max)
+    })
 }
-
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search').addEventListener('click', getWeather)
